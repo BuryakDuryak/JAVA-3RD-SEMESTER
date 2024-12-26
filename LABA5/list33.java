fc22afc19da0b140b330a14b5fbf6060c92d6720
Buryat <59414511+BuryakDuryak@users.noreply.github.com>
2024-12-26 12:33:11 +0500

    Update list33.java

--- LABA5/list33.java
+++ LABA5/list33.java
@@ -5,15 +5,15 @@ public class list33 {
 
     public static <T extends Comparable<T>> List<T> mergeSortedLists(List<T> list1, List<T> list2) {
         if (list1 == null) {
-            return list2; // Если первый список null, то возвращаем второй
+            return list2; 
         }
         if (list2 == null) {
-            return list1; // Если второй список null, то возвращаем первый
+            return list1; 
         }
 
         List<T> mergedList = new ArrayList<>();
-        int i = 0; // Индекс для list1
-        int j = 0; // Индекс для list2
+        int i = 0; 
+        int j = 0; 
 
         while (i < list1.size() && j < list2.size()) {
             if (list1.get(i).compareTo(list2.get(j)) <= 0) {
@@ -25,13 +25,11 @@ public class list33 {
             }
         }
 
-        // Добавляем оставшиеся элементы из list1 (если есть)
         while (i < list1.size()) {
             mergedList.add(list1.get(i));
             i++;
         }
 
-        // Добавляем оставшиеся элементы из list2 (если есть)
         while (j < list2.size()) {
             mergedList.add(list2.get(j));
             j++;
